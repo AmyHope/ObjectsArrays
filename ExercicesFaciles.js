b993d2d3c007d38f03c9cfbd925f7f8c2ec6f143
AmyHope <ksefiani1@gmail.com>
2024-11-20 09:18:06 +0100

    exercice 3

--- ExercicesFaciles.js
+++ ExercicesFaciles.js
@@ -4,3 +4,9 @@ console.log(nombres);
 // Exercice 2
 let fruits = ["apple", "banana", "cherry"];
 console.log(fruits[1]);
+// Exercice 3
+let numbers = [10, 20, 30, 40];
+numbers [2]= 50;
+console.log(numbers);
+
+
