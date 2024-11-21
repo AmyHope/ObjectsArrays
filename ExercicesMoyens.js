f913045cbd7bd0fffe9a2d76819ef948989c3193
AmyHope <ksefiani1@gmail.com>
2024-11-21 10:41:38 +0100

    Ex 11 meth 1

---
+++ ExercicesMoyens.js
@@ -0,0 +1,9 @@
+//Ex 11 meth 1
+let numbers = [1, 2, 3, 4, 5];
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
+//Ex 11 meth 2
+numbers = [1, 2, 3, 4, 5];
+for (numbers key )
+
